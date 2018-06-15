32f17981bb314aa979250b9ebdfef6ba7fcee3d1
Eddie Harris <harrisejm@gmail.com>
2018-06-15 15:36:15 -0700

    fix styling formating for outputing final price

---
+++ README.md
@@ -0,0 +1,42 @@
+# Eddie's Epicodus Pizza Online Orders Project
+
+#### Epicodus Pizza Ordering Project (for Epicodus), 6/15/18
+#### By Eddie Harris
+
+
+## Description
+
+This is a webpage allows you to order pizza's online. You can order as many pizzas and as many combinations as you like.
+
+## Specifications
+
+- 1.) Determine if customer would like to order take out or delivery.
+
+- 2.) If customer selects delivery, provide them with a field to enter their address.
+
+- 3.) Assign a value to each pizza size and each pizza topping. Based of the customers selection, add the values to get the total price for the pizza. Add values of each pizza if more than one are selected. Add sales tax and output total.
+
+    - Example input: A medium pizza ($13.99) with Bacon ($2.00) with Black Olives ($2.00) and a large pizza ($17.99) with extra cheese ($2.00)
+    - Example output: $41.93 w/ tax
+
+
+## Setup/Installation Requirements
+
+* Clone his repository
+* Save to Desktop
+* Open index.html and run in web browser
+
+
+## Technologies Used
+
+HTML
+CSS
+JavaScript
+
+### License
+
+This software is licensed under the MIT license
+
+###Legal
+
+Copyright (c) 2018 Eddie Harris
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-  var test = "";
   var priceTotal = 0;
   var pricePizza = 0;
 
@@ -32,73 +31,67 @@ $(document).ready(function() {
     return "<strong>" + "$" + pricePizza.toFixed(2) + " : " + this.size + "</strong>" + " pizza with " + this.toppings;
   }
 
-  $("#formOne").submit(function(event) {
-    event.preventDefault();
-/////Values
-   var name = $("input#name").val();
-   var street = $("input.street").val();
-   var city = $("input.city").val();
-   var state = $("input.state").val();
-    var zip = $("input.zip").val();
-
-
-   var size = $("input:radio[name=size]:checked").val();
-//////
-
-
-  var order1 = new Orders(name, size);
-
-  $("input:checkbox[name=toppings]:checked").each(function(){
-   order1.toppings.push(" " + $(this).val());
-  });
-  if (order1.toppings.length === 0){
-    order1.toppings[0] = " Cheese";
-  }
-
-  order1.calc();
-
-   var tax = (priceTotal / 100) * 10.4;
-document.getElementById("outPutName").innerHTML = order1.name;
-document.getElementById("outPutName2").innerHTML = order1.name;
-
-document.getElementById("outPutAddress").innerHTML = street + "</br>" + city + " " + state + ", " + zip;
-document.getElementById("outPutAddress2").innerHTML = street + "</br>" + city + " " + state + ", " + zip;
-
-document.getElementById("outPutTotalPrice").innerHTML = "Total: $" + priceTotal.toFixed(2);
-$("ul#orderInfo").append("<li><span class='pizzaInfo'>" + order1.displayOrder() + "</span></li>");
-
-
-$("ul#orderInfo2").append("<li><span class='pizzaInfo'>" + order1.displayOrder() + "</span></li>");
-
-
-document.getElementById("outPutTotalPrice2").innerHTML = "Price: $" + priceTotal.toFixed(2);
-document.getElementById("tax").innerHTML = "Tax: $" + tax.toFixed(2);
-document.getElementById("finalPrice").innerHTML = "Total: $" + (priceTotal + tax).toFixed(2);
-
-  });
-
   $("button.delivery").click(function() {
-     $(".address").show();
-     $(".pizzaOrder").show();
-     $(".orderButton1").show();
-      $(".orderCheckOut").show();
-     $(".takeOut").hide();
-     $(".delivery").hide();
+    $(".nameC").show();
+    $(".address").show();
+    $(".pizzaOrder").show();
+    $(".orderButton1").show();
+    $(".takeOut").hide();
+    $(".delivery").hide();
+    $(".intro").hide();
 
-    });
+  });
+  $("button.takeOut").click(function() {
+    $(".nameC").show();
+    $(".pizzaOrder").show();
+    $(".orderButton1").show();
+    $(".takeOut").hide();
+    $(".delivery").hide();
+    $(".intro").hide();
+  });
 
-    $("button.takeOut").click(function() {
-      $(".pizzaOrder").show();
-      $(".orderButton1").show();
-      $(".orderCheckOut").show();
-       $(".takeOut").hide();
-       $(".delivery").hide();
-      });
-
-      $("button.checkOut").click(function() {
+  $("button.checkOut").click(function() {
     $(".orderConfirm").show();
     $(".row").hide();
+  });
 
-   });
+
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    /////Values
+    var name = $("input#name").val();
+    var street = $("input.street").val();
+    var city = $("input.city").val();
+    var state = $("input.state").val();
+    var zip = $("input.zip").val();
+    var size = $("input:radio[name=size]:checked").val();
+
+    var order1 = new Orders(name, size);
+
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      order1.toppings.push(" " + $(this).val());
+    });
+    if (order1.toppings.length === 0){
+      order1.toppings[0] = " Cheese";
+    }
+
+    order1.calc();
+
+    var tax = (priceTotal / 100) * 10.4;
+
+    document.getElementById("outPutName").innerHTML = order1.name;
+    document.getElementById("outPutName2").innerHTML = order1.name;
+    document.getElementById("outPutAddress").innerHTML = street + "</br>" + city + " " + state + ", " + zip;
+    document.getElementById("outPutAddress2").innerHTML = street + "</br>" + city + " " + state + ", " + zip;
+    document.getElementById("outPutTotalPrice").innerHTML = "Total: $" + priceTotal.toFixed(2);
+    document.getElementById("outPutTotalPrice2").innerHTML = "Price: $" + priceTotal.toFixed(2);
+    document.getElementById("tax").innerHTML = "Tax: $" + tax.toFixed(2);
+    document.getElementById("finalPrice").innerHTML = "Total: $" + (priceTotal + tax).toFixed(2);
+    $("ul#orderInfo").append("<li><span class='pizzaInfo'>" + order1.displayOrder() + "</span></li>");
+    $("ul#orderInfo2").append("<li><span class='pizzaInfo'>" + order1.displayOrder() + "</span></li>");
+    
+    $(".orderCheckOut").show();
+  });
+
 
 });
