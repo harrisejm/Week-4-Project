81d127cc8c7fa37faea910045acb652fe213b495
Eddie Harris <harrisejm@gmail.com>
2018-06-15 09:48:44 -0700

    add user interface and price counter in JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,49 @@
 $(document).ready(function() {
-var Orders = function(name, size, toppings) {
-  this.name = name;
-  this.size = size;
-  this.toppings = toppings;
-}
 
-var order1 = new Orders("Eddie", "12 inches", "Cheese");
+  var test = "";
+  var price = 0;
+
+  var Orders = function(name, size, toppings) {
+    this.name = name;
+    this.size = size;
+    this.toppings = [];
+  }
+
+  Orders.prototype.calc = function(){
+    if (this.size === "small") {
+      price += 10;
+    } else if (this.size === "medium") {
+      price += 14;
+    } else if (this.size === "large") {
+      price += 17;
+    } else if (this.size === "giant") {
+      price += 30;
+    }
+
+    price += this.toppings.length * 2;
+
+
+  }
+
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+/////Values
+   var name = $("input#name").val();
+   var size = $("input:radio[name=size]:checked").val();
+//////
+  var order1 = new Orders(name, size);
+
+  $("input:checkbox[name=toppings]:checked").each(function(){
+   order1.toppings.push($(this).val());
+  });
+
+
+   order1.calc();
+
+document.getElementById("test").innerHTML = price;
 
 
 
-
-document.getElementById("test").innerHTML = order1.toppings;
-
+  });
 
 });
